d4f72deeaa7cf6b9c28fccbf033b3ef087104a55
smart dev <smart.dev.989@gmail.com>
2025-03-08 09:11:00 +0200

    delete unneeded user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
-
-import { User } from './users/user.entity'; // Import the User entity
 import { Book } from './books/book.entity';
 import { AuthorsModule } from './authors/authors.module';
 import { Author } from './authors/author.entity';
@@ -19,10 +16,9 @@ import { Author } from './authors/author.entity';
       username: 'root',   // Your MariaDB username
       password: 'root', // Your MariaDB password
       database: 'nestjs',  // Name of your MariaDB database
-      entities: [User,Book,Author],
+      entities: [Book,Author],
       synchronize: true,  // Automatically create database schema
     }),
-    UsersModule,  // Importing the Users module
     BooksModule,
     AuthorsModule
   ],
